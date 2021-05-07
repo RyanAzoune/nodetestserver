1b49c168d619569b7908a92f2d3c50399c79147b
Ryan Azoune <ryan.azoune@ssense.com>
2021-05-07 11:53:53 -0400

    Put request in a function so that every time we go to localhost we get a new person

--- server.js
+++ server.js
@@ -4,14 +4,17 @@ const port = 80
 const request = require('request')
 var person
 
-request('https://randomuser.me/api/', function (error, response, body) {
-  if(!error && response.statusCode == 200) {
-    person = JSON.parse(body)
-    person.results[0]["jobs"] = "fullstack dev at ssense"
-  }
-})
+function getPersonInfo() {
+  request('https://randomuser.me/api/', function (error, response, body) {
+    if(!error && response.statusCode == 200) {
+      person = JSON.parse(body)
+      person.results[0]["jobs"] = "fullstack dev at ssense"
+    }
+  })
+}
 
 app.get('/', (req, res) => {
+  getPersonInfo()
   res.send(person)
 })
 
