92ea265c4f65c0fff9c1fbbc870ef053bbe02bf0
Ryan Azoune <ryan.azoune@ssense.com>
2021-05-07 11:16:22 -0400

    Add a new field to randomuser.me/api

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express')
 const app = express()
 const port = 80
+const request = require('request')
+var person
+
+request('https://randomuser.me/api/', function (error, response, body) {
+  if(!error && response.statusCode == 200) {
+    person = JSON.parse(body)
+    person.results[0]["jobs"] = "fullstack dev at ssense"
+  }
+})
 
 app.get('/', (req, res) => {
-    var d = new Date()
-    var time = "Time: " + d.getHours() + ':' + d.getMinutes()
-  res.send(time)
+  res.send(person)
 })
 
 app.listen(port, () => {
