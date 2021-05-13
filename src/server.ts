b1ce1a76832d3c9bb6e419183c8d671b6bd8cbfc
RyanAzoune <66181407+RyanAzoune@users.noreply.github.com>
2021-05-13 17:36:22 -0400

    Adding fetch library
    Replacing request with fetch

--- src/server.ts
+++ src/server.ts
@@ -4,39 +4,52 @@ const app = express();
 const port = 80;
 const request = require('request');
 const _ = require('lodash')
+const fetch = require('node-fetch')
+
+
+
 
 app.get('/', (req:Request, res:Response) => {
   var baseURL = 'https://randomuser.me/api/?'
   const gender = req.query.gender
   const nat = req.query.nat
   const seed = req.query.seed
-  if(gender != undefined) 
-  {
-    baseURL += "gender=" + gender + "&";
-  }
-  if(nat != undefined)
-  {
-    baseURL += "nat=" + nat + "&";
-  }
-  if(seed != undefined)
-  {
-    baseURL += "seed=" + seed + "&";
-  }
-  request(baseURL, function (error:ErrorRequestHandler, response:Response, body:Request["body"]) {
-    if(!error && response.statusCode == 200) {
-      body = JSON.parse(body);
-      const person:Person = body.results[0];
-      person["jobs"] = "fullstack dev at sssense";
-      if(!req.query.field) {
-        res.send(person);
-        return;
-      }
-      const fields = (req.query.field as string).split(",");
-      const filtered = _.pick(person, fields);
-      res.send(filtered);
+  // if(gender != undefined) 
+  // {
+  //   baseURL += "gender=" + gender + "&";
+  // }
+  // if(nat != undefined)
+  // {
+  //   baseURL += "nat=" + nat + "&";
+  // }
+  // if(seed != undefined)
+  // {
+  //   baseURL += "seed=" + seed + "&";
+  // }
+  
+  fetch('https://randomuser.me/api/?')
+  .then(res => res.json())
+  .then(json => console.log(json));
 
-    }
-  })
+
+
+
+
+  // request(baseURL, function (error:ErrorRequestHandler, response:Response, body:Request["body"]) {
+  //   if(!error && response.statusCode == 200) {
+  //     body = JSON.parse(body);
+  //     const person:Person = body.results[0];
+  //     person["jobs"] = "fullstack dev at sssense";
+  //     if(!req.query.field) {
+  //       res.send(person);
+  //       return;
+  //     }
+  //     const fields = (req.query.field as string).split(",");
+  //     const filtered = _.pick(person, fields);
+  //     res.send(filtered);
+
+  //   }
+  // })
 })
 
 app.listen(port, () => {
