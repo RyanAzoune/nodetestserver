6494a717fc00c58d7393500a248d55013bb98d33
RyanAzoune <66181407+RyanAzoune@users.noreply.github.com>
2021-05-13 11:01:42 -0400

    If no field paramaters, return Person

--- src/server.ts
+++ src/server.ts
@@ -7,15 +7,17 @@ const request = require('request');
 app.get('/', (req:Request, res:Response) => {
   request('https://randomuser.me/api/', function (error:ErrorRequestHandler, response:Response, body:Request["body"]) {
     if(!error && response.statusCode == 200) {
-      if(!req.query.field) {
-        res.send({});
-        return;
-      }
-      const fields = new Set(String(req.query.field).split(","));
-
       body = JSON.parse(body);
       const person:Person = body.results[0];
       person["jobs"] = "fullstack dev at sssense";
+
+      if(!req.query.field) {
+        res.send(person);
+        return;
+      }
+
+      const fields = new Set(String(req.query.field).split(","));
+
       for(var k in person) {
         if(!(fields.has(k))) {
           delete person[k as keyof Person];
