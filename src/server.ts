617f28e532873cc1c15b8a2d0146bf514bb1cfea
RyanAzoune <66181407+RyanAzoune@users.noreply.github.com>
2021-05-17 08:57:02 -0400

    Added async function with fetch library
    Added async function to use keyword await

--- src/server.ts
+++ src/server.ts
@@ -1,55 +1,29 @@
-import express, { Request, Response, ErrorRequestHandler } from 'express'
-import { Person } from './types'
+import express, { Request, Response, ErrorRequestHandler } from 'express';
+import { Person } from './types';
+import fetch from 'node-fetch';
+import omitBy from 'lodash/omitBy';
+import isEmpty from 'lodash/isEmpty';
+import _ from 'lodash';
 const app = express();
 const port = 80;
-const request = require('request');
-const _ = require('lodash')
-const fetch = require('node-fetch')
 
 
-
-
-app.get('/', (req:Request, res:Response) => {
-  var baseURL = 'https://randomuser.me/api/?'
-  const gender = req.query.gender
-  const nat = req.query.nat
-  const seed = req.query.seed
-  // if(gender != undefined) 
-  // {
-  //   baseURL += "gender=" + gender + "&";
-  // }
-  // if(nat != undefined)
-  // {
-  //   baseURL += "nat=" + nat + "&";
-  // }
-  // if(seed != undefined)
-  // {
-  //   baseURL += "seed=" + seed + "&";
-  // }
-  
-  fetch('https://randomuser.me/api/?')
-  .then(res => res.json())
-  .then(json => console.log(json));
-
-
-
-
-
-  // request(baseURL, function (error:ErrorRequestHandler, response:Response, body:Request["body"]) {
-  //   if(!error && response.statusCode == 200) {
-  //     body = JSON.parse(body);
-  //     const person:Person = body.results[0];
-  //     person["jobs"] = "fullstack dev at sssense";
-  //     if(!req.query.field) {
-  //       res.send(person);
-  //       return;
-  //     }
-  //     const fields = (req.query.field as string).split(",");
-  //     const filtered = _.pick(person, fields);
-  //     res.send(filtered);
-
-  //   }
-  // })
+app.get('/', async (req:Request, res:Response) => {
+  const params = new URLSearchParams(omitBy({ gender: req.query.gender, nat: req.query.nat, seed: req.query.seed }, isEmpty));
+  const url = new URL('https://randomuser.me/api/');
+  url.search = new URLSearchParams(params).toString();
+  const response = await fetch(url);
+  const data = await response.json();
+  const person:Person = data.results[0];
+  person["jobs"] = "fullstack dev at ssense";
+  var filtered;
+  if(!req.query.field) {
+   filtered = person;
+  } else {
+    const fields = (req.query.field as string).split(',');
+    filtered = _.pick(person, fields);
+  }
+  res.send(filtered);
 })
 
 app.listen(port, () => {
